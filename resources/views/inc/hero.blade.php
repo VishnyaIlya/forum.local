96f275e4b6fa1a262498780ecff950d14ca1d8a4
VishnyaIlya <96049735+VishnyaIlya@users.noreply.github.com>
2021-12-16 21:05:23 +0400

    forum

---
+++ resources/views/inc/hero.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1>Привет всем на нашем сайте!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+</div>
